70f015591fe39930393ad59a52449188fdad8f0c
Romulo Banhe <romulobanhe@microsoft.com>
2022-03-03 14:52:37 -0300

    SQL Insert Sample

---
+++ insert into `customers`(`customerNumber`.sql
@@ -0,0 +1,5 @@
+insert  into `customers`(`customerNumber`,`customerName`,`contactLastName`,`contactFirstName`,`phone`,`addressLine1`,`addressLine2`,`city`,`state`,`postalCode`,`country`,`salesRepEmployeeNumber`,`creditLimit`) values 
+(500,'Romulo Banhe Argentina','Kiumi','Akamka ','11995823452','Rua Angelo Conradini',NULL,'Jundiai',NULL,'13202100','Argentina',1370,'18000.00')
+
+
+DELETE FROM classicmodels.customers where customerNumber = 500 
